d84c5e1555b02e76d8e33ed052dd2184a63e34c3
Yashashvi Dave <yashashvidave@gmail.com>
2020-02-29 17:20:12 -0600

    Few changes with the graph style.

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_graph(x, y, style, xlabel, ylabel, title, label='', legend=None):
+def plot_graph(x, y, style, xlabel, ylabel, title, label='', legend=None, pointlabel=False):
     plt.plot(x, y, style, label=label)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
     if legend:
       plt.legend()
-
-    for a,b in zip(x[::5], y[::5]):
-        plt.text(a+0.2, b-0.5, str(b))
+    
+    if pointlabel:
+        for a,b in zip(x[::5], y[::5]):
+            plt.text(a+0.2, b-0.5, str(b))
 
 
 # Load data from CSV downloaded from NCES(https://nces.ed.gov/programs/digest/d18/tables/dt18_105.30.asp)
@@ -52,12 +53,16 @@ y = data['Total Enrollment'][12:].values
 y1 = data['Elementary & Secondary enrollment'][12:].values
 y2 = data['Degree-granting postsecondary institutions Total'][12:].values
 
-plot_graph(x, y, '-og', "Year", "Total Enrollment(in thousands)", "Enrollment trend in U.S.")
+fig, axs = plt.subplots(nrows=1, figsize=(10, 7))
+plot_graph(x, y, '-og', "Year", "Total Enrollment(in thousands)", "Enrollment trend in U.S.", pointlabel=True)
 plt.show()
-plot_graph(x, y1, '-ob', "Year", "Total Enrollment(in thousands) in Elementary and Secondary", "Enrollment trend in U.S.")
+fig, axs = plt.subplots(nrows=1, figsize=(10, 7))
+plot_graph(x, y1, '-ob', "Year", "Total Enrollment(in thousands) in Elementary and Secondary", "Enrollment trend in U.S.", pointlabel=True)
 plt.show()
-plot_graph(x, y2, '-or', "Year", "Total Enrollment(in thousands) in Postgraduation", "Enrollment trend in U.S.")
+fig, axs = plt.subplots(nrows=1, figsize=(10, 7))
+plot_graph(x, y2, '-or', "Year", "Total Enrollment(in thousands) in Postgraduation", "Enrollment trend in U.S.", pointlabel=True)
 plt.show()
+
 
 
 # Comapre enrollment growth over the years between postgraduation and secodary graduation
@@ -66,6 +71,7 @@ y = data['Total Enrollment'].pct_change()[13:].values
 y1 = data['Elementary & Secondary enrollment'].pct_change()[13:].values
 y2 = data['Degree-granting postsecondary institutions Total'].pct_change()[13:].values
 
+fig, axs = plt.subplots(nrows=1, figsize=(10, 7))
 plot_graph(x, y, '-og', "Year", "Total Enrollment Growth in %", "Enrollment trend in U.S.", label='Total Enrollment')
 plot_graph(x, y1, '-ob', "Year", "Total Enrollment Growth in %", "Enrollment trend in U.S.",
           label="Elementary and Secondary")
